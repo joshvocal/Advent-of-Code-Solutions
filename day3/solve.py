8573f3e3dfd94ae1598022300b02612848ee463b
Josh Vocal <joshvocal@gmail.com>
2019-12-18 14:08:57 -0800

    Add d3 p2

--- day3/solve.py
+++ day3/solve.py
@@ -1,9 +1,8 @@
-def part1(w1, w2):
-
-    def helper(ww):
+def helper(ww):
         x = 0
         y = 0
-        s = set()
+        steps = 0
+        s = dict()
 
         for w in ww:
             a = w[0]
@@ -12,21 +11,31 @@ def part1(w1, w2):
             if a == 'U':
                 for i in range(b):
                     y += 1
-                    s.add((x, y))
+                    steps += 1
+                    if (x, y) not in s or s[(x, y)] < steps:
+                        s[(x, y)] = steps
             elif a == 'D':
                 for i in range(b):
                     y -= 1
-                    s.add((x, y))
+                    steps += 1
+                    if (x, y) not in s or s[(x, y)] < steps:
+                        s[(x, y)] = steps
             elif a == 'L':
                 for i in range(b):
                     x -= 1
-                    s.add((x, y))
+                    steps += 1
+                    if (x, y) not in s or s[(x, y)] < steps:
+                        s[(x, y)] = steps
             elif a == 'R':
                 for i in range(b):
                     x += 1
-                    s.add((x, y))
+                    steps += 1
+                    if (x, y) not in s or s[(x, y)] < steps:
+                        s[(x, y)] = steps
 
         return s
+
+def part1(w1, w2):
 
     s1 = helper(w1)
     s2 = helper(w2)
@@ -40,10 +49,21 @@ def part1(w1, w2):
 
     return min(l)
 
+def part2(w1, w2):
+    s1 = helper(w1)
+    s2 = helper(w2)
+    j = [i for i in s1 if i in s2]
+    l = []
+    for a in j:
+        l.append(s1[a] + s2[a])
+
+    return min(l)
+
 with open('inputs.txt', 'r') as f:
     lines = f.readlines()
     w1 = lines[0].strip().split(',')
     w2 = lines[1].strip().split(',')
 
     print(part1(w1, w2))
+    print(part2(w1, w2))
 
