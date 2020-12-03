ab6e94ed6f3dcbc8836be687547801cc06ae973a
Josh Vocal <joshvocal@gmail.com>
2020-12-03 08:45:32 -0800

    Day 3

---
+++ 2020/src/day3/TobogganTrajectory.kt
@@ -0,0 +1,28 @@
+package day3
+
+import java.io.File
+
+fun main() {
+    val input: List<String> = File("src/day3/input.txt").readLines()
+
+    println(solvePart1(input))
+    println(solvePart2(input))
+}
+
+fun solvePart1(input: List<String>): Int =
+    input.countTrees(3, 1)
+
+fun solvePart2(input: List<String>): Long =
+    listOf(1 to 1, 3 to 1, 5 to 1, 7 to 1, 1 to 2)
+        .map { input.countTrees(it.first, it.second) }
+        .reduce { total, trees -> total * trees }
+        .toLong()
+
+fun List<String>.countTrees(right: Int, down: Int): Int {
+    var x = 0
+
+    return (down until size step down).count { y ->
+        x += right
+        this[y][x % this[y].length] == '#'
+    }
+}
