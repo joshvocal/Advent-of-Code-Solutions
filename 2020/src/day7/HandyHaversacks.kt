aadaf6a15ff89af44e6a5771411163ba66e38798
Josh Vocal <joshvocal@gmail.com>
2020-12-12 18:31:57 -0800

    Day 7: Part 1

--- 2020/src/day7/HandyHaversacks.kt
+++ 2020/src/day7/HandyHaversacks.kt
@@ -4,27 +4,109 @@ import java.io.File
 
 fun main() {
     val input = File("src/day7/sample_input.txt").readLines()
-    solvePart1(input)
+
+    println(solvePart1(input))
+//    println(solvePart2(input))
 }
 
 private fun solvePart1(input: List<String>): Int {
 
+    val map = hashMapOf<String, ArrayList<String>>()
+    val list = mutableSetOf<String>()
+    val tempList: MutableList<String>
+
     for (line in input) {
-        println(line)
 
-        val (key, value) = line.split("contain")
-        println("$key $value")
+        val key = line.substringBefore("contain").trim()
+        val value = line.substringAfter("contain").trim()
 
-//        val a = line.split(' ')
-//            .filter { !it.contains("bag") }
-//            .joinToString(" ")
+        val newKey = key.replace("bag[s]?".toRegex(), "")
+            .trim()
+        val newValue = value.replace(" bag[s]?[^,]?".toRegex(), "")
+            .replace(" ?[0-9] ".toRegex(), "")
+            .trim()
+            .split(",")
 
+        if (!map.containsKey(newKey)) {
+            map[newKey] = arrayListOf()
+        }
 
+        map[newKey]?.addAll(newValue)
     }
 
-    return 0
+    map.forEach { (key, value) ->
+        if (value.contains("shiny gold")) {
+            list.add(key)
+        }
+    }
+
+    tempList = list.toMutableList()
+
+    // BFS
+    while (tempList.isNotEmpty()) {
+        for (i in 0 until tempList.size) {
+            val curr = tempList.removeFirst()
+
+            map.forEach { (key, value) ->
+                if (curr in value && key !in list) {
+                    tempList.add(key)
+                    list.add(key)
+                }
+            }
+        }
+    }
+
+    return list.size
 }
 
-private fun solvePart2(passports: List<String>): Int {
-    return 0
+private fun solvePart2(input: List<String>): Int {
+
+    fun String.splitAtIndex(index: Int) = take(index) to substring(index)
+
+    val map = hashMapOf<String, ArrayList<Pair<String, String>>>()
+    val list = arrayListOf<Pair<String, String>>()
+    val tempList: MutableList<Pair<String, String>>
+
+    for (line in input) {
+        val key = line.substringBefore("contain").trim()
+        val value = line.substringAfter("contain").trim()
+
+        val newKey = key.replace("bag[s]?".toRegex(), "")
+            .trim()
+        val newValue = value.replace(" bag[s]?[^,]?".toRegex(), "")
+            .trim()
+            .split(", ")
+            .map {
+                val (first, second) = it.splitAtIndex(it.indexOf(' '))
+                Pair(first.trim(), second.trim())
+            }
+
+        if (!map.containsKey(newKey)) {
+            map[newKey] = arrayListOf()
+        }
+
+        map[newKey]?.addAll(newValue)
+    }
+
+    map["shiny gold"]?.let { list.addAll(it) }
+
+    tempList = list.toMutableList()
+
+    // BFS
+    while (tempList.isNotEmpty()) {
+        for (i in 0 until tempList.size) {
+            val curr = tempList.removeFirst()
+
+            map.forEach { (key, value) ->
+                val (num, color) = curr
+                println("$num $color")
+//                if (curr in value && key !in list) {
+//                    tempList.add(key)
+//                    list.add(key)
+//                }
+            }
+        }
+    }
+
+    return list.size
 }
