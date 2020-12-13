0ea064f889d89808095d30c1672f2cea00721b20
Josh Vocal <joshvocal@gmail.com>
2020-12-13 11:53:43 -0800

    Day 2: Cleaner

--- 2020/src/day2/PasswordPhilosophy.kt
+++ 2020/src/day2/PasswordPhilosophy.kt
@@ -3,44 +3,33 @@ package day2
 import java.io.File
 
 fun main() {
-    val lines: List<String> = File("src/day2/input.txt").readLines()
+    val input: List<String> = File("src/day2/input.txt").readLines()
+    val passwords = parsePasswords(input)
 
-    println(solvePart1(lines))
-    println(solvePart2(lines))
+    println(solvePart1(passwords))
+    println(solvePart2(passwords))
 }
 
-private fun solvePart1(lines: List<String>): Int {
-    var numValidPasswords = 0
-
-    lines.forEach { line ->
-        val lower = line.substringBefore('-').toInt()
-        val upper = line.substring(line.indexOf('-') + 1, line.indexOf(' ')).toInt()
-        val char = line.substring(line.indexOf(' '), line.indexOf(':'))
-        val password = line.substringAfterLast(' ')
-
-        val count = password.filter { char.contains(it) }.count()
-
-        if (count in lower..upper) {
-            numValidPasswords += 1
-        }
-    }
-
-    return numValidPasswords
+private data class PasswordPolicy(val range: IntRange, val char: Char, val password: String) {
+    fun isOldValid() = password.count { char -> char == this.char } in range.first..range.last
+    fun isNewValid() = (char == password[range.first - 1]) xor (char == password[range.last - 1])
 }
 
-private fun solvePart2(lines: List<String>): Int {
-    var numValidPasswords = 0
+private fun parsePasswords(input: List<String>): List<PasswordPolicy> {
+    return input.map { line ->
+        val lower: Int = line.substringBefore('-').toInt()
+        val upper: Int = line.substring(line.indexOf('-') + 1, line.indexOf(' ')).toInt()
+        val char: Char = line.substring(line.indexOf(' '), line.indexOf(':')).trim().single()
+        val password: String = line.substringAfterLast(' ')
 
-    lines.forEach { line ->
-        val lower = line.substringBefore('-').toInt()
-        val upper = line.substring(line.indexOf('-') + 1, line.indexOf(' ')).toInt()
-        val char = line.substring(line.indexOf(' '), line.indexOf(':'))
-        val password = line.substringAfterLast(' ')
-
-        if (char.contains(password[lower - 1]) xor char.contains(password[upper - 1])) {
-            numValidPasswords += 1
-        }
+        PasswordPolicy(lower..upper, char, password)
     }
+}
 
-    return numValidPasswords
+private fun solvePart1(passwords: List<PasswordPolicy>): Int {
+    return passwords.count { password -> password.isOldValid() }
+}
+
+private fun solvePart2(passwords: List<PasswordPolicy>): Int {
+    return passwords.count { password -> password.isNewValid() }
 }
