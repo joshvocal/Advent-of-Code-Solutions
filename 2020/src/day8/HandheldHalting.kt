e4e9e9e360be21f694a8aa76285361ea3e33b3d7
Josh Vocal <joshvocal@gmail.com>
2020-12-13 11:56:29 -0800

    Day 8: Pre

---
+++ 2020/src/day8/HandheldHalting.kt
@@ -0,0 +1,15 @@
+package day8
+
+import java.io.File
+
+fun main() {
+    val input = File("src/day8/sample_input.txt").readLines()
+}
+
+private fun solvePart1(input: List<String>): Int {
+    return 0
+}
+
+private fun solvePart2(passports: List<String>): Int {
+    return 0
+}
