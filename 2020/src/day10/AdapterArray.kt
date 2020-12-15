3c85b25c3cea95b409e8b4274057aa6f521f7065
Josh Vocal <joshvocal@gmail.com>
2020-12-15 14:40:43 -0800

    Day 10: Pre

---
+++ 2020/src/day10/AdapterArray.kt
@@ -0,0 +1,15 @@
+package day10
+
+import java.io.File
+
+fun main() {
+    val input = File("src/day10/sample_input.txt").readLines()
+}
+
+private fun solvePart1(input: List<String>): Int {
+    return 0
+}
+
+private fun solvePart2(passports: List<String>): Int {
+    return 0
+}
