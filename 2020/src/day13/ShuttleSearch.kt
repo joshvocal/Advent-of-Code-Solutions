b017a661e05512a87a7f947e1f234940dc043d5c
Josh Vocal <joshvocal@gmail.com>
2020-12-16 10:36:01 -0800

    Day 13: Part 1

--- 2020/src/day13/ShuttleSearch.kt
+++ 2020/src/day13/ShuttleSearch.kt
@@ -1,13 +1,37 @@
 package day13
 
 import java.io.File
+import kotlin.math.floor
 
 fun main() {
-    val input = File("src/day13/sample_input.txt").readLines()
+    val input = File("src/day13/input.txt").readLines()
+
+    val earliestTime = input[0].toInt()
+    val busIds = input[1].split(",")
+        .filter { busId -> busId != "x" }
+        .map { it.toInt() }
+
+    println(solvePart1(earliestTime, busIds))
 }
 
-private fun solvePart1(input: List<String>): Int {
-    return 0
+private fun solvePart1(earliestTime: Int, busIds: List<Int>): Int {
+    val map = hashMapOf<Int, Int>()
+    var min = Int.MAX_VALUE
+    var earliestBusId = busIds[0]
+
+    busIds.forEach {
+        val earliestBus = (floor(earliestTime / it.toDouble()) + 1) * it
+        map[it] = earliestBus.toInt()
+    }
+
+    map.forEach { (key, value) ->
+        if (value - earliestTime < min) {
+            earliestBusId = key
+            min = value - earliestTime
+        }
+    }
+
+    return earliestBusId * min
 }
 
 private fun solvePart2(passports: List<String>): Int {
