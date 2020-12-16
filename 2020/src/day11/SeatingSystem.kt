94e0c58f3f46f0dfa64bdbc266309dc652ed3647
Josh Vocal <joshvocal@gmail.com>
2020-12-16 06:38:04 -0800

    Day 11: Part 2

--- 2020/src/day11/SeatingSystem.kt
+++ 2020/src/day11/SeatingSystem.kt
@@ -6,15 +6,17 @@ fun main() {
     val input = File("src/day11/input.txt").readLines()
 
     println(solvePart1(input).joinToString("").count { it == OCCUPIED })
+    println(solvePart2(input).joinToString("").count { it == OCCUPIED })
 }
 
 const val EMPTY = 'L'
+const val FLOOR = '.'
 const val OCCUPIED = '#'
 
-fun solvePart1(chairs: List<String>): List<String> {
-    val newSeats = newSeats(chairs)
+fun solvePart1(seats: List<String>): List<String> {
+    val newSeats = newSeats(seats)
 
-    if (newSeats == chairs) {
+    if (newSeats == seats) {
         return newSeats
     }
 
@@ -34,7 +36,6 @@ fun newSeats(floor: List<String>): List<String> {
     }
 }
 
-
 private fun getNeighbours(x: Int, y: Int, chairs: List<String>): List<Char?> =
     listOf(
         Pair(-1, -1), // Top-left
@@ -45,8 +46,55 @@ private fun getNeighbours(x: Int, y: Int, chairs: List<String>): List<Char?> =
         Pair(-1, 1), // Bottom-left
         Pair(0, 1), // Bottom
         Pair(1, 1), // Bottom-right
-    ).map { chairs.getOrNull(y + it.second)?.getOrNull(x + it.first) }
+    ).map { coordinates ->
+        val (x2, y2) = coordinates
+        chairs.getOrNull(y + y2)?.getOrNull(x + x2)
+    }
 
-private fun solvePart2(passports: List<String>): Int {
-    return 0
+private fun solvePart2(seats: List<String>): List<String> {
+    val newSeats = newSeats2(seats)
+
+    if (newSeats == seats) {
+        return newSeats
+    }
+
+    return solvePart2(newSeats)
 }
+
+fun newSeats2(floor: List<String>): List<String> {
+    return floor.mapIndexed { y, row ->
+        row.mapIndexed { x, seat ->
+            val neighbours = getNeighbours2(x, y, floor)
+            when {
+                seat == EMPTY && neighbours.count { it == OCCUPIED } == 0 -> OCCUPIED
+                seat == OCCUPIED && neighbours.count { it == OCCUPIED } >= 5 -> EMPTY
+                else -> seat
+            }
+        }.joinToString("")
+    }
+}
+
+private fun getNeighbours2(x: Int, y: Int, chairs: List<String>): List<Char?> =
+    listOf(
+        Pair(-1, -1), // Top-left
+        Pair(0, -1), // Top
+        Pair(1, -1), // Top-right
+        Pair(-1, 0), // Left
+        Pair(1, 0), // Right
+        Pair(-1, 1), // Bottom-left
+        Pair(0, 1), // Bottom
+        Pair(1, 1), // Bottom-right
+    ).map { coordinates ->
+        val (x2, y2) = coordinates
+        var range = 0
+        var seat: Char? = chairs.getOrNull(y + y2)?.getOrNull(x + x2)
+
+        while (seat == FLOOR) {
+            range += 1
+            val xSight = (x2 * range)
+            val ySight = (y2 * range)
+            seat = chairs.getOrNull(y + ySight)?.getOrNull(x + xSight)
+        }
+
+        seat
+    }
