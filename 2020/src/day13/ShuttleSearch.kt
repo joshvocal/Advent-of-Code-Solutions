8bb460f45c6f53ab77295a01b54b0d8711715931
Josh Vocal <joshvocal@gmail.com>
2020-12-16 10:13:22 -0800

    Day 13: Pre

---
+++ 2020/src/day13/ShuttleSearch.kt
@@ -0,0 +1,15 @@
+package day13
+
+import java.io.File
+
+fun main() {
+    val input = File("src/day13/sample_input.txt").readLines()
+}
+
+private fun solvePart1(input: List<String>): Int {
+    return 0
+}
+
+private fun solvePart2(passports: List<String>): Int {
+    return 0
+}
