90135e6e234b8bf9aa7ca18d908640c843a4a85a
Josh Vocal <joshvocal@gmail.com>
2020-12-15 16:48:50 -0800

    Day 10: Part 2 TODO

--- 2020/src/day10/AdapterArray.kt
+++ 2020/src/day10/AdapterArray.kt
@@ -2,14 +2,19 @@ package day10
 
 import java.io.File
 import java.util.*
+import kotlin.collections.ArrayList
 
 fun main() {
-    val input = File("src/day10/input.txt").readLines()
+    val input = File("src/day10/sample_input.txt").readLines()
     val adapters: List<Int> = input.map { it.toInt() }
 
     println(solvePart1(adapters))
+    println(solvePart2(adapters))
 }
 
+/**
+ * Min Heap
+ */
 private fun solvePart1(adapters: List<Int>): Int {
     val pq: PriorityQueue<Int> = PriorityQueue(adapters)
     var diffs = 0
@@ -33,6 +38,37 @@ private fun solvePart1(adapters: List<Int>): Int {
     return numThreeJoltDif * numOneJoltDif
 }
 
-private fun solvePart2(passports: List<String>): Int {
-    return 0
+private fun solvePart2(adapters: List<Int>): Int {
+    val numbers = adapters.sorted()
+
+    val res = arrayListOf<ArrayDeque<Int>>()
+    dfs(numbers, res, ArrayDeque<Int>())
+
+    println(res)
+
+    return res.size
+}
+
+/**
+ * TODO: Solve with Backtracking and Permutations? DP?
+ */
+private fun dfs(numbers: List<Int>, res: ArrayList<ArrayDeque<Int>>, temp: ArrayDeque<Int>) {
+    if (temp.size > 3) {
+        return
+    }
+
+    if (temp.size == 3) {
+        res.add(ArrayDeque(temp))
+        return
+    }
+
+    for (num in numbers) {
+        if (num !in temp) {
+            temp.add(num)
+            dfs(numbers, res, temp)
+            temp.pop()
+        }
+    }
+
+    return
 }
