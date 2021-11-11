915cf34e84e5e640e9931487701f92799f90a0f2
Josh Vocal <joshvocal@gmail.com>
2021-11-11 09:33:47 -0800

    Add Day 6 Part 1 2015

---
+++ 2015/day_06/solve.py
@@ -0,0 +1,40 @@
+def part1(lines):
+    grid = [[0 for _ in range(1000)] for _ in range(1000)]
+
+    for line in lines:
+        instruction, from_coords, _, dest_coords = line.strip().rsplit(' ', 3)
+        from_coords = [int(coord) for coord in from_coords.split(',')]
+        dest_coords = [int(coord) for coord in dest_coords.split(',')]
+
+        for x_coord in range(from_coords[0], dest_coords[0] + 1):
+            for y_coord in range(from_coords[1], dest_coords[1] + 1):
+                light = grid[x_coord][y_coord]
+
+                if instruction == "turn on":
+                    if not light:
+                        grid[x_coord][y_coord] = 1
+                elif instruction == "turn off":
+                    if light:
+                        grid[x_coord][y_coord] = 0
+                else:
+                    if light:
+                        grid[x_coord][y_coord] = 0
+                    else:
+                        grid[x_coord][y_coord] = 1
+
+    num_lights = 0
+
+    for row in grid:
+        for col in row:
+            if col == 1:
+                num_lights += 1
+
+    return num_lights
+
+def part2(lines):
+    return
+
+with open('input.txt', 'r') as f:
+    lines = f.read().splitlines()
+
+    print(part1(lines))
