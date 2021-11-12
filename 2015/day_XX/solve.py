9d990c68635763a2d0b1fe1d644eed80fc8461ec
Josh Vocal <joshvocal@gmail.com>
2021-11-11 21:59:26 -0800

    Add template

---
+++ 2015/day_XX/solve.py
@@ -0,0 +1,11 @@
+def part1(lines):
+    return
+
+def part2(lines):
+    return
+
+with open("input.txt", "r") as f:
+    lines = f.read().splitlines()
+
+    print(part1(lines))
+    print(part2(lines))
