477a5ebf4ab53ca5a2e3132b3c2857bf609524bf
Josh Vocal <joshvocal@gmail.com>
2022-07-10 07:11:22 -0700

    Refactoring

--- 2016/day_01/solve.py
+++ 2016/day_01/solve.py
@@ -1,37 +1,45 @@
 from enum import Enum
+import sys
 
-class Direction(Enum):
-    NORTH = (0, 1)
-    EAST = (1, 0)
-    SOUTH = (0, -1)
-    WEST = (-1, 0)
+class Solution:
+    class Direction(Enum):
+        NORTH = (0, 1)
+        EAST = (1, 0)
+        SOUTH = (0, -1)
+        WEST = (-1, 0)
 
-COORDINATES = [Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST]
+    COORDINATES = [Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST]
 
-def part1(instructions):
-    direction = 0
-    current_coordinates = (0, 0)
+    def part1(self, instructions):
+        direction = 0
+        current_coordinates = (0, 0)
 
-    for instruction in instructions:
-        turn, steps = instruction
+        for instruction in instructions:
+            turn, steps = instruction
 
-        if turn == 'R':
-            direction = (direction + 1) % len(COORDINATES)
-        elif turn == 'L':
-            direction = (direction - 1) % len(COORDINATES)
+            if turn == 'R':
+                direction = (direction + 1) % len(self.COORDINATES)
+            elif turn == 'L':
+                direction = (direction - 1) % len(self.COORDINATES)
 
-        new_coordinates = tuple([axis * steps for axis in COORDINATES[direction].value])
-        current_coordinates = tuple(map(sum, zip(new_coordinates, current_coordinates)))
+            new_coordinates = tuple([axis * steps for axis in self.COORDINATES[direction].value])
+            current_coordinates = tuple(map(sum, zip(new_coordinates, current_coordinates)))
 
-    return sum(map(abs, current_coordinates))
+        return sum(map(abs, current_coordinates))
 
-def part2(lines):
-    return
+    def part2(self, lines):
+        return
 
-with open('input.txt', 'r') as f:
-    instructions = f.readline().split(', ')
+def main(textfile):
+    with open(textfile, 'r') as f:
+        instructions = f.readline().split(', ')
 
-    instructions = [(instruction[0], int(instruction[1:])) for instruction in instructions]
+        instructions = [(instruction[0], int(instruction[1:])) for instruction in instructions]
 
-    print(part1(instructions))
-    print(part2(instructions))
+        sol_1 = Solution()
+
+        print(sol_1.part1(instructions))
+        print(sol_1.part2(instructions))
+
+if __name__ == "__main__":
+    main(sys.argv[1])
--- 2016/solve.py
+++ 2016/solve.py
@@ -1,11 +1,20 @@
-def part1(lines):
-    return
+import sys
 
-def part2(lines):
-    return
+class Solution:
+    def part1(self, lines):
+        return
 
-with open('input.txt', 'r') as f:
-    lines = f.read().splitlines()
+    def part2(self, lines):
+        return
 
-    print(part1(lines))
-    print(part2(lines))
+def main(textfile):
+    with open(textfile, 'r') as f:
+        lines = f.read().splitlines()
+
+        sol_1 = Solution()
+
+        print(sol_1.part1(lines))
+        print(sol_1.part2(lines))
+
+if __name__ == "__main__":
+    main(sys.argv[1])
