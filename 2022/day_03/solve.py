9d25969f91c6327cd69eda6067f9b37683c2c3ab
Josh Vocal <joshvocal@gmail.com>
2022-12-02 21:47:44 -0800

    Add Day 3 Part 2

--- 2022/day_03/solve.py
+++ 2022/day_03/solve.py
@@ -10,7 +10,7 @@ class Solution:
 
             size = len(line)
             first, second = set(line[:size//2]), set(line[size//2:])
-
+            
             for char in first:
                 if char in second:
                     if char.isupper():
@@ -18,25 +18,36 @@ class Solution:
                     else:
                         priority_sum += ord(char) - ord('a') + 1
 
-                    second.remove(char)
-
         return priority_sum
 
     def part2(self, lines):
-        return None
+        freq = {}
+        priority_sum = 0
+
+        for group in zip(lines, lines, lines):
+            for rucksack in group:
+                for char in set(rucksack):
+                    freq[char] = freq.get(char, 0) + 1
+
+                    if freq[char] == 3:
+                        if char.isupper():
+                            priority_sum += ord(char) - ord('A') + 27
+                        else:
+                            priority_sum += ord(char) - ord('a') + 1
+            freq = {}
+
+        return priority_sum
 
 
 def main(textfile):
     with open(textfile, "r") as f:
         lines = f.read().splitlines()
-        aa = [line for line in f][:3]
-
-        print(aa)
+        iter_lines = iter(lines)
 
         sol_1 = Solution()
 
         print("Part 1: ", sol_1.part1(lines))
-        print("Part 2: ", sol_1.part2(lines))
+        print("Part 2: ", sol_1.part2(iter_lines))
 
 
 if __name__ == "__main__":
